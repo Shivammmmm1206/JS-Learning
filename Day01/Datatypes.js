8cc52c8cc039480316637595cf16bede13a2b6a9
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-05 16:13:55 +0530

    This is lesson 2 about Datatypes

---
+++ Day01/Datatypes.js
@@ -0,0 +1,30 @@
+// this is lesson 2 about Datatypes in JavaScript
+// today we will learn about primitive data types
+
+// first is Number
+//Number = represents Numeric values
+
+let n = 42;
+let pi = 3.14;
+
+console.log(n)
+console.log(pi)
+
+// String
+// Represents text enclosed in single or double quotes
+
+let s = "Shivam";
+console.log(s)
+
+// Boolean
+// Represents a logical value either true or false
+
+let bool =  true
+console.log(bool)
+
+//undefined
+// A variable that has been declared but not assigned a value
+
+let notAssigned;
+console.log(notAssigned)
+
