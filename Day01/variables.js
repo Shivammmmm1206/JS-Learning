95a6eb907084a5f08abf3603f5ec23686892421c
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-05 16:04:27 +0530

    this is lesson 1 about variables

---
+++ Day01/variables.js
@@ -0,0 +1,24 @@
+// in this lesson we will learn about how to create variables in JS
+// a variable is like a container that holds data
+// in JS variables are declared using var,let or const keyword
+
+var n = 5;
+console.log(n)
+// the variable which is created using Var keyword can reassign its value
+
+var n = 20;
+console.log(n)
+
+// the next one is let keyword
+// let keyword cant be re-assigned in the same scope
+// we can update te value as well but not in the same scope
+
+let m = 10;
+console.log(m)
+
+// the final one and most used keyword is Const
+// const keyword declares variable that cannot be reassigned
+
+const k  =  100;
+//k =  150; this will throw the error
+console.log(k)
