6c5e4c65e67d43d4cec0bcc935c27e245fab4f2a
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-05 16:41:11 +0530

    this is lesson 3 about operators we talked about arithmetic and assignment operators today

---
+++ Day01/Operators.js
@@ -0,0 +1,51 @@
+// lesson-3 about operators in JS
+// Arithmetic operators
+// used to perform arithmetic operations like addition,substraction,multiplication
+
+let A =  10 + 10  // the "+" is used for addition operations
+console.log (A)
+
+let B =  15 - 10   // the "-" is used for substraction operations
+console.log (B)
+
+let C  =  15 * 9  // the "*" is used for multiplication operations
+console.log (C)
+
+let D  =  50/5  //  the "/" is used for division operations
+console.log(D)
+
+// increment operator
+
+let E = 7
+E++ // it increase the value by 1
+console.log(E)
+
+// decrement operator
+
+let F =  9
+F-- // it decrease the value by 1
+console.log(F)
+
+// Assignment operators
+
+// addition assignment operations
+
+let G =  10
+H =   G += 10 //  it adds the value to right operand to  a variable and assign the result to variable
+console.log(H)
+
+
+// Substraction assignment operators
+let yoo = 4;
+
+// Expected output 3
+console.log(foo = yoo - 1);
+
+// Multiplication assignment operators
+let  i =  5 ;
+console.log(i *= 2)
+
+// Divison assignment operator
+
+let   j  = 10;
+console.log(j /= 2)
