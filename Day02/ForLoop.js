8c8ee0591cc7e480c89f16b3b9818035f4a8eef5
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-06 22:00:03 +0530

    For loop

---
+++ Day02/ForLoop.js
@@ -0,0 +1,15 @@
+// loops
+// we will learn about For-loop
+// JavaScript for loop is a control flow statement that allows code to be executed repeatedly based on a condition.
+
+// here is example of for loop
+
+// for loop to print table of number
+
+// syntax of for loop
+// for (statement 1 ; statement 2 ; statement 3){    code here...}
+
+ let x = 5
+for (let i = 1; i<=10; i++) {
+    console.log(x*i)
+}
