13276db1668f19d1f0025e99447cb4188f13b1e2
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-07 23:54:53 +0530

    learning about Do-While loops

---
+++ Day03/DoWhileLoop.js
@@ -0,0 +1,17 @@
+// we will learn about do while loop in this lesson
+
+//A do…while loop in JavaScript is a control structure where the code executes repeatedly based on a given boolean condition.
+// there are two types of loops
+// Entry control loop
+// exit control loop
+// do while loop is exit control loop
+//do {
+//     // Statements
+// }
+// while(conditions)
+
+let test =  1;
+do {
+    console.log(test);
+    test++;
+} while (test <=5)
