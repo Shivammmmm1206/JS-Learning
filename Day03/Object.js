d749635a4e82587e672283c7ce69ae2df310a987
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-08 00:00:05 +0530

    learning about objects in javascript it sounds hard needed revison as well

---
+++ Day03/Object.js
@@ -0,0 +1,25 @@
+//An object in JavaScript is a data structure used to store related data collections
+
+// there are two ways to create object in Javascript
+
+// object literal
+// object constructor
+
+// first i will demonstrate object literal
+
+let obj = {
+    name : "shivam",
+    age : 20,
+    gender : "male"
+};
+
+console.log(obj);
+
+// second i will demonstrate object constructor
+
+let obj2 =  new Object();
+obj2.name = "shivam";
+obj2.age = 20;
+obj2.gender = "male";
+
+console.log(obj2);
