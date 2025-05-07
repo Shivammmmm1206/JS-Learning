b7b3c0dc101597a13906382d47218ac3fc412aa9
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-07 23:44:34 +0530

    starting day 3 with while loops

---
+++ Day03/WhileLoop.js
@@ -0,0 +1,11 @@
+// today we will learn about while loop
+
+// The while loop executes a block of code as long as a specified condition is true.
+
+// here is example of while loop that executes to print 1 to 5
+
+let count = 1;
+while(count <=5) {
+    console.log(count);
+    count++;
+}
