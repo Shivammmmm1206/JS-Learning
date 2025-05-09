d0ab365dcf5baa338e7d7ac22e5b03337a383c5b
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-09 23:58:22 +0530

    we are learning about encapsulation in this chapter

---
+++ Day04/Encapsulation.js
@@ -0,0 +1,21 @@
+// we will learn about Encapsulation in this lesson
+
+// The process of wrapping properties and functions within a single unit is known as encapsulation.
+
+class person {
+    constructor(name , id) {
+        this.name = name;
+        this.id = id;
+    }
+    add_Address(add) {
+        this.address = add;
+    }
+    getDetails() {
+        console.log(`Name is ${this.name},
+        Address is ${this.add}`);
+    }
+}
+
+let person1 = new person('Mukul', 21);
+person1.add_Address('Delhi');
+person1.getDetails();
