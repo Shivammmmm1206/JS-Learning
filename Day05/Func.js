fde8c2499fba0514e75c79934c1aaa18988a6164
Shivammmmm1206 <shivamzxz8@gmail.com>
2025-05-11 22:32:00 +0530

    Starting day with functions in JS

---
+++ Day05/Func.js
@@ -0,0 +1,11 @@
+// today we will learn about functions
+
+// Functions in Javascript are reusable block of code which is used to perform a specific task.
+
+// basic function
+
+function sum(x,y) {
+    return x + y;
+}
+
+console.log(sum(10,15));  // this is the basic function in JS
